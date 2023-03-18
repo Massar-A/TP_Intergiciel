916099f62db88a4f3128a269069192c11ffaa200
Massar <massarabbas62@gmail.com>
2023-03-18 01:58:52 +0100

    modified docker-compose.yml, Dockerfile and application.properties to match ip adresses of other containers

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:17-jdk-alpine3.14
 WORKDIR /app
 COPY ./target/*.jar /app/
-CMD ["sh", "-c", "java -jar /app/*.jar"]
+CMD ["sh", "-c", "java -jar /app/Producer1-0.0.1-SNAPSHOT.jar & java -jar /app/consumer2producer3-0.0.1-SNAPSHOT.jar & java -jar /app/consumer3producer2-0.0.1-SNAPSHOT.jar & java -jar /app/Consumer1-0.0.1-SNAPSHOT.jar"]
--- consumer3producer2/src/main/java/com/insa/consumer3producer2/Producer2.java
+++ consumer3producer2/src/main/java/com/insa/consumer3producer2/Producer2.java
@@ -22,7 +22,7 @@ public class Producer2 {
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         // See https://kafka.apache.org/documentation/#producerconfigs for more properties
